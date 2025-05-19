05f7392aed9d5affe3cf6031592828b02fa82f94
Weminsoo <weminsoo@naver.com>
2025-05-19 19:19:45 +0900

    gitaction을 위한 editor 파일 추가

---
+++ Assets/Editor/SetKeystoreSettings.cs
@@ -0,0 +1,14 @@
+using UnityEditor;
+
+public class SetKeystoreSettings
+{
+    [MenuItem("CI/ApplyKeystoreSettings")]
+    public static void Apply()
+    {
+        PlayerSettings.Android.useCustomKeystore = true;
+        PlayerSettings.Android.keystoreName = "user.keystore";
+        PlayerSettings.Android.keystorePass = System.Environment.GetEnvironmentVariable("KEYSTORE_PASSWORD");
+        PlayerSettings.Android.keyaliasName = System.Environment.GetEnvironmentVariable("KEY_ALIAS");
+        PlayerSettings.Android.keyaliasPass = System.Environment.GetEnvironmentVariable("KEY_PASSWORD");
+    }
+}
